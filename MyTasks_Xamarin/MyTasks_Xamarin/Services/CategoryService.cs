365f7c5e01ec3ce507d93ffc4a549b2fb8ff6cf8
Jakub Zięba <jakubzieba7@gmail.com>
2024-01-14 11:40:27 +0100

    Added CategoryService and ICategoryService

---
+++ MyTasks_Xamarin/MyTasks_Xamarin/Services/CategoryService.cs
@@ -0,0 +1,64 @@
+﻿using MyTasks_WebAPI.Core.DTOs;
+using MyTasks_WebAPI.Core.Response;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTasks_Xamarin.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private static readonly HttpClient _httpClient = new HttpClient { BaseAddress = new Uri(App.BackendUrl) };
+
+        public async Task<DataResponse<int>> AddCategoryAsync(CategoryDto category)
+        {
+            var stringContent = new StringContent(JsonConvert.SerializeObject(category), Encoding.UTF8, "application/json");
+
+            using (var response = await _httpClient.PostAsync("category", stringContent))
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<DataResponse<int>>(responseContent);
+            }
+        }
+
+        public async Task<Response> UpdateCategoryAsync(CategoryDto category)
+        {
+            var stringContent = new StringContent(JsonConvert.SerializeObject(category), Encoding.UTF8, "application/json");
+
+            using (var response = await _httpClient.PutAsync("category", stringContent))
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<Response>(responseContent);
+            }
+        }
+
+        public async Task<Response> DeleteCategoryAsync(int id)
+        {
+            using (var response = await _httpClient.DeleteAsync($"category/{id}"))
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<Response>(responseContent);
+            }
+        }
+
+        public async Task<DataResponse<TaskDto>> GetCategoryAsync(int id)
+        {
+            var json = await _httpClient.GetStringAsync($"category/{id}");
+
+            return JsonConvert.DeserializeObject<DataResponse<TaskDto>>(json);
+        }
+
+        public async Task<DataResponse<IEnumerable<TaskDto>>> GetCategoriesAsync()
+        {
+            var json = await _httpClient.GetStringAsync("category/");
+
+            return JsonConvert.DeserializeObject<DataResponse<IEnumerable<TaskDto>>>(json);
+        }
+    }
+}
---
+++ MyTasks_Xamarin/MyTasks_Xamarin/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using MyTasks_WebAPI.Core.DTOs;
+using MyTasks_WebAPI.Core.Response;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyTasks_Xamarin.Services
+{
+    internal interface ICategoryService
+    {
+        Task<DataResponse<int>> AddCategoryAsync(CategoryDto category);
+        Task<Response> UpdateCategoryAsync(CategoryDto category);
+        Task<Response> DeleteCategoryAsync(int id);
+        Task<DataResponse<TaskDto>> GetCategoryAsync(int id);
+        Task<DataResponse<IEnumerable<TaskDto>>> GetCategoriesAsync();
+    }
+}
